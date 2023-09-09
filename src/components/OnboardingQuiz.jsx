f643f2ea07297078d081f733a7f0fcff51afab92
bennetthilberg <bennett.hilberg@gmail.com>
2023-09-09 15:23:12 -0500

    Some setup for OnboardingQuiz

--- src/components/OnboardingQuiz.jsx
+++ src/components/OnboardingQuiz.jsx
@@ -2,13 +2,22 @@ import React, {useEffect, useState} from "react";
 import { useAtom } from "jotai";
 import { onboardingUserAtom } from "../globalAtoms";
 import { Form, Input, Select, AutoComplete,Button } from "antd";
+import Typography from 'antd/es/typography'
+import '../secondarystyles/onboardingQuiz.scss' // *not using App.scss
 
 export default function OnboardingQuiz() {
     const [onboardingUser, setOnboardingUser] = useAtom(onboardingUserAtom);
     console.log(onboardingUser);
     return(
-        <div id='onboardingQuizScreen'>
-
+        <div id='onboardingQuizComp'>
+            <div id="promptHolder">
+                <Typography>
+                    hi im prompt
+                </Typography>
+            </div>
+            <div id="answerMenu">
+                
+            </div>
         </div>
     );
 }
