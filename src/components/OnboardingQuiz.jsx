875e477c552ee62477e2115cf32cba9d87087333
bennetthilberg <bennett.hilberg@gmail.com>
2023-09-24 17:05:47 -0500

    Make quiz questions transition smoothly into next quiz questio **with bug - it goes to like a white screen first (otherwise works tho)

--- src/components/OnboardingQuiz.jsx
+++ src/components/OnboardingQuiz.jsx
@@ -6,7 +6,7 @@ import Typography from 'antd/es/typography'
 import '../secondarystyles/onboardingQuiz.scss' // *not using App.scss
 import AnswerMenu from "./AnswerMenu";
 import PromptHolder from "./PromptHolder";
-import { motion } from "framer-motion";
+import { AnimatePresence,motion } from "framer-motion";
 import { slideInBouncy, slideOutBouncy } from "../transitions";
 
 // keep options short, 1-3 words
@@ -50,39 +50,62 @@ export default function OnboardingQuiz() {
         quizTracker -> quizTracker.globalCurrentQuestion -> progress -> currentQuestion
     */
 
+    const [showNextQuestion, setShowNextQuestion] = useState(false);
+
+    useEffect(() => {
+        if(showNextQuestion){
+            setQuizTracker((prev) => ({...prev, globalCurrentQuestion: prev.globalCurrentQuestion + 1}));
+            setShowNextQuestion(false);
+        }
+    }, [showNextQuestion, setQuizTracker]);
+    useEffect(() => {
+        console.log("Current progress:", progress);
+    }, [progress]);
+    useEffect(() => {
+        console.log("showNextQuestion changed:", showNextQuestion);
+    }, [showNextQuestion]);
+    
+    
+ 
     function onAnswerSelect(answer) {
         // 'answer' is the index of the answer selected in currentQuestion.options array
         console.log('answer arg is:');
         console.log(answer);
         console.log(`user answered option ${answer} which is '${currentQuestion.options[answer]}'`)
-        setQuizTracker(oldQuizTracker => {
+        /*setQuizTracker(oldQuizTracker => {
             return {
                 ...oldQuizTracker,
                 globalCurrentQuestion: oldQuizTracker.globalCurrentQuestion + 1
             }
-        })
+        })* this is now done in useEffect, remove when sure it works*/
+        setShowNextQuestion(true);
     }
 
     return (
-        <motion.div id='onboardingQuizComp'
-            animate="animate"
-            initial="initial"
-            exit="exit"
-            variants={slideInBouncy}
-        >
-            <PromptHolder
-                prompt={currentQuestion.prompt}
-                promptFigure={currentQuestion.promptFigure}
-            />
-
-
-            <AnswerMenu
-                options={currentQuestion.options}
-                figures={currentQuestion.figures}
-                onAnswerSelect={onAnswerSelect}
-            />
-
-
+        <motion.div id='onboardingQuizComp'>
+            <AnimatePresence mode='wait'>
+                {quizPrompts[progress] && ( // Check if the question exists
+                    <motion.div 
+                    className='onboardingQuizAnimatePresence'
+                        key={progress}
+                        animate="animate"
+                        initial="initial"
+                        exit="exit"
+                        variants={slideInBouncy}
+                    >
+                        <PromptHolder
+                            prompt={quizPrompts[progress].prompt}
+                            promptFigure={quizPrompts[progress].promptFigure}
+                        />
+                        <AnswerMenu
+                            options={quizPrompts[progress].options}
+                            figures={quizPrompts[progress].figures}
+                            onAnswerSelect={onAnswerSelect}
+                        />
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </motion.div>
     );
+    
 }
