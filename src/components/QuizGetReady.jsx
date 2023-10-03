2d43f5fdd7e829a0e2c5d0724dd69365300cbdbe
bennetthilberg <bennett.hilberg@gmail.com>
2023-10-03 11:53:43 -0500

    Finish QGR styling, add image for QGR and modify its colors

--- src/components/QuizGetReady.jsx
+++ src/components/QuizGetReady.jsx
@@ -3,8 +3,8 @@ import { motion } from "framer-motion";
 import { slideInBouncy, slideOutBouncy } from "../transitions";
 import { Button, Typography, Steps } from "antd";
 
-export default function QuizGetReady({nextStep}) {
-    function handleBegin(){
+export default function QuizGetReady({ nextStep }) {
+    function handleBegin() {
         nextStep();
     }
     return (
@@ -14,17 +14,25 @@ export default function QuizGetReady({nextStep}) {
             exit="exit"
             variants={slideInBouncy}
         >
-            {/* Put a quiz/matching type thing icon here */}
-            <Typography  className="onboardingFormSubtext oInfoTitle">
-                Time for a quiz!
-            </Typography>
-            <Typography className="onboardingFormSubtext oInfosubTitle" >
-                We'll ask you a few questions to help match you with the perfect oppurtunities.
-            </Typography>
-            
-            <Button id='qgrButton' type="primary" size="large">
-                Begin
-            </Button>
+            <div className="qgrImageHolder">
+                <img src="src/assets/quizthing.svg" className="qgrImage" />
+            </div>
+
+            <div className="qgrContent">
+                <Typography className="onboardingFormSubtext oInfoTitle">
+                    Time for a quiz!
+                </Typography>
+                <Typography className="onboardingFormSubtext oInfosubTitle" >
+                    We'll ask you a few questions to help match you with the perfect oppurtunities.
+                </Typography>
+
+                <Button id='qgrButton' type="primary" size="large"
+                    onClick={handleBegin}
+                >
+                    Begin
+                </Button>
+            </div>
+
         </motion.div>
     )
 }
